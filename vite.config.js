e8ca4a783e4a76eabb1c1f424e36191f6f0e9175
piskesocute <happyday9837@gmail.com>
2024-02-02 23:26:33 +0800

    feat: 調整環境網址

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/LTCS/' : '/',
   plugins: [
     vue(),
     UnoCSS({
